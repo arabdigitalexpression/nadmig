2dd11d7e4bbfaafb745776cdac40780e165558c5
Mohamed Mahrous <m.mahrous.94@gmail.com>
2016-06-28 12:14:50 +0200

    admin controller debug

--- app/Base/Controllers/AdminController.php
+++ app/Base/Controllers/AdminController.php
@@ -69,7 +69,13 @@ abstract class AdminController extends Controller
             $method = 'POST';
             $path = $this->viewPath("create");
         }
-
+         dd( FormBuilder::create($this->formPath, [
+                'method' => $method,
+                'url' => $url,
+                'model' => $object
+            ], [
+                $extra,
+            ]));
         $form = $this->createForm($url, $method, $object, $extra);
         
         return view($path, compact('form', 'object', 'extra'));
