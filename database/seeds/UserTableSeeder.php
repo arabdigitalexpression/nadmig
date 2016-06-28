06f1d8aab8e0af015753f1e148a8669e60256a43
Mohamed Mahrous <m.mahrous.94@gmail.com>
2016-06-28 05:34:18 +0200

    add confirm to user seeds

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -13,6 +13,6 @@ class UserTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->delete();
-        User::create(['name' => 'Admin', 'email' => 'm.mahrous@arabdigitalexpression.org', 'password' => '123456','birthday' => '14-2-1994', 'governorate' => 'giza']);
+        User::create(['name' => 'Admin', 'email' => 'm.mahrous@arabdigitalexpression.org', 'password' => '123456','birthday' => '14-2-1994', 'governorate' => 'giza', 'confirmed' => 1]);
     }
 }
