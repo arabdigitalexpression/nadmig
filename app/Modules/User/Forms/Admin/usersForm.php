10e5afe3b911326edb7bcb43b9ba1d9fa3cf99f0
Mohamed Mahrous <m.mahrous.94@gmail.com>
2016-06-28 06:40:14 +0200

    user form edit debug

--- app/Modules/User/Forms/Admin/usersForm.php
+++ app/Modules/User/Forms/Admin/usersForm.php
@@ -40,13 +40,13 @@ class UsersForm extends AdminForm
             ->add('instagram', 'text', [
                 'label' => trans('User::dashboard.fields.user.instagram')
             ])
-            ->add('role', 'choice', [
-                'choices' => $this->getRoles(),
-                'selected' => [],
-                'label' => trans('User::dashboard.fields.user.role'),
-                'expanded' => true,
-                'multiple' => true
-            ])
+            // ->add('role', 'choice', [
+            //     'choices' => $this->getRoles(),
+            //     'selected' => [],
+            //     'label' => trans('User::dashboard.fields.user.role'),
+            //     'expanded' => true,
+            //     'multiple' => true
+            // ])
             ->add('picture', 'file', [
                 'label' => trans('User::dashboard.fields.user.picture'),
                 'attr' => ['class' => '']
