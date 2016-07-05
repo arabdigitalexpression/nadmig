3693127d63da74aa53a245eda60709e00012ce89
Mohamed Mahrous <m.mahrous.94@gmail.com>
2016-07-05 05:27:30 +0200

    update slug config

--- config/sluggable.php
+++ config/sluggable.php
@@ -46,75 +46,31 @@ return array(
      *
      *      'method' => array('Str','slug'),
      */
-    'method' => function($string, $separator = '-')
-        {
-            $_transliteration = array(
-                '/ä|æ|ǽ/' => 'ae',
-                '/ö|œ/' => 'oe',
-                '/ü/' => 'ue',
-                '/Ä/' => 'Ae',
-                '/Ü/' => 'Ue',
-                '/Ö/' => 'Oe',
-                '/À|Á|Â|Ã|Å|Ǻ|Ā|Ă|Ą|Ǎ/' => 'A',
-                '/à|á|â|ã|å|ǻ|ā|ă|ą|ǎ|ª/' => 'a',
-                '/Ç|Ć|Ĉ|Ċ|Č/' => 'C',
-                '/ç|ć|ĉ|ċ|č/' => 'c',
-                '/Ð|Ď|Đ/' => 'D',
-                '/ð|ď|đ/' => 'd',
-                '/È|É|Ê|Ë|Ē|Ĕ|Ė|Ę|Ě/' => 'E',
-                '/è|é|ê|ë|ē|ĕ|ė|ę|ě/' => 'e',
-                '/Ĝ|Ğ|Ġ|Ģ/' => 'G',
-                '/ĝ|ğ|ġ|ģ/' => 'g',
-                '/Ĥ|Ħ/' => 'H',
-                '/ĥ|ħ/' => 'h',
-                '/Ì|Í|Î|Ï|Ĩ|Ī|Ĭ|Ǐ|Į|İ/' => 'I',
-                '/ì|í|î|ï|ĩ|ī|ĭ|ǐ|į|ı/' => 'i',
-                '/Ĵ/' => 'J',
-                '/ĵ/' => 'j',
-                '/Ķ/' => 'K',
-                '/ķ/' => 'k',
-                '/Ĺ|Ļ|Ľ|Ŀ|Ł/' => 'L',
-                '/ĺ|ļ|ľ|ŀ|ł/' => 'l',
-                '/Ñ|Ń|Ņ|Ň/' => 'N',
-                '/ñ|ń|ņ|ň|ŉ/' => 'n',
-                '/Ò|Ó|Ô|Õ|Ō|Ŏ|Ǒ|Ő|Ơ|Ø|Ǿ/' => 'O',
-                '/ò|ó|ô|õ|ō|ŏ|ǒ|ő|ơ|ø|ǿ|º/' => 'o',
-                '/Ŕ|Ŗ|Ř/' => 'R',
-                '/ŕ|ŗ|ř/' => 'r',
-                '/Ś|Ŝ|Ş|Ș|Š/' => 'S',
-                '/ś|ŝ|ş|ș|š|ſ/' => 's',
-                '/Ţ|Ț|Ť|Ŧ/' => 'T',
-                '/ţ|ț|ť|ŧ/' => 't',
-                '/Ù|Ú|Û|Ũ|Ū|Ŭ|Ů|Ű|Ų|Ư|Ǔ|Ǖ|Ǘ|Ǚ|Ǜ/' => 'U',
-                '/ù|ú|û|ũ|ū|ŭ|ů|ű|ų|ư|ǔ|ǖ|ǘ|ǚ|ǜ/' => 'u',
-                '/Ý|Ÿ|Ŷ/' => 'Y',
-                '/ý|ÿ|ŷ/' => 'y',
-                '/Ŵ/' => 'W',
-                '/ŵ/' => 'w',
-                '/Ź|Ż|Ž/' => 'Z',
-                '/ź|ż|ž/' => 'z',
-                '/Æ|Ǽ/' => 'AE',
-                '/ß/' => 'ss',
-                '/Ĳ/' => 'IJ',
-                '/ĳ/' => 'ij',
-                '/Œ/' => 'OE',
-                '/ƒ/' => 'f'
-            );
+    'method' => function ($string, $separator = '-') {
+        if (is_null($string)) {
+            return "";
+        }
 
+        // Remove spaces from the beginning and from the end of the string
+        $string = trim($string);
 
-            $quotedReplacement = preg_quote($separator, '/');
+        // Lower case everything
+        // using mb_strtolower() function is important for non-Latin UTF-8 string | more info: http://goo.gl/QL2tzK
+        $string = mb_strtolower($string, "UTF-8");;
 
-            $merge = array(
-                '/[^\s\p{Zs}\p{Ll}\p{Lm}\p{Lo}\p{Lt}\p{Lu}\p{Nd}]/mu' => ' ',
-                '/[\s\p{Zs}]+/mu' => $separator,
-                sprintf('/^[%s]+|[%s]+$/', $quotedReplacement, $quotedReplacement) => '',
-            );
+        // Make alphanumeric (removes all other characters)
+        // this makes the string safe especially when used as a part of a URL
+        // this keeps latin characters and arabic charactrs as well
+        $string = preg_replace("/[^a-z0-9_\s-ءاأإآؤئبتثجحخدذرزسشصضطظعغفقكلمنهويةى]#u/", "", $string);
 
-            $map = $_transliteration + $merge;
-            unset($_transliteration);
+        // Remove multiple dashes or whitespaces
+        $string = preg_replace("/[\s-]+/", " ", $string);
 
-            return mb_strtolower(preg_replace(array_keys($map), array_values($map), $string));
-        },
+        // Convert whitespaces and underscore to the given separator
+        $string = preg_replace("/[\s_]/", $separator, $string);
+
+        return $string;
+    },
 
     /**
      * Separator to use if using the default Str::slug() method.  Defaults to a hyphen.
@@ -145,7 +101,7 @@ return array(
      * re-saved (i.e. already exists).  Defaults to false, which
      * means slugs are not updated.
      */
-    'on_update' => false,
+    'on_update' => true,
 
     /**
      * An array of slug names that can never be used for this model,
