c9cbcd8e5e6fca923eee5b3dbbe8c11669ba728f
Mohamed Mahrous <m.mahrous.94@gmail.com>
2016-07-07 11:22:03 +0200

    add /school to trainer

--- app/Modules/Trainer/routes.php
+++ app/Modules/Trainer/routes.php
@@ -3,10 +3,10 @@
 Route::group(['middleware' => 'web'], function () {
     // Application routes
     Route::group(['module' => 'Trainer', 'namespace' => 'Application'], function () {
-        Route::get('trainers', ['as' => 'trainers', 'uses' => 'TrainerController@all']);
-        Route::get('trainer/{trainer_slug}', ['as' => 'trainer.page', 'uses' => 'TrainerController@index']);
-        Route::get('trainer', ['as' => 'trainer', 'uses' => 'TrainerController@me']);
-        Route::get('trainer/{trainer_slug}/edit', ['as' => 'application.trainer.edit', 'uses' => 'TrainerController@edit']);
+        Route::get('school/trainers', ['as' => 'trainers', 'uses' => 'TrainerController@all']);
+        Route::get('school/trainer/{trainer_slug}', ['as' => 'trainer.page', 'uses' => 'TrainerController@index']);
+        Route::get('school/trainer', ['as' => 'trainer', 'uses' => 'TrainerController@me']);
+        Route::get('school/trainer/{trainer_slug}/edit', ['as' => 'application.trainer.edit', 'uses' => 'TrainerController@edit']);
     });
 });
 
