bb090507fd101ef0cd5642e4376f1fc865d930de
Mohamed Mahrous <m.mahrous.94@gmail.com>
2016-07-13 16:48:39 +0200

    fix CreateTrainerReportsTable

--- database/migrations/2016_07_13_143411_create_trainer_reports_table.php
+++ database/migrations/2016_07_13_143411_create_trainer_reports_table.php
@@ -12,26 +12,28 @@ class CreateTrainerReportsTable extends Migration
      */
     public function up()
     {
-        $table->increments('id');
-            $table->integer('event_id')->unsigned();
-            $table->foreign('event_id')->references('id')->on('events');
-            $table->integer('attendees_id')->unsigned();
-            $table->foreign('attendees_id')->references('id')->on('attendees');
-            $table->integer('trainer_id')->unsigned();
-            $table->foreign('trainer_id')->references('id')->on('trainers');
-            $table->integer('week');
-            $table->text('confidence');
-            $table->text('initiative');
-            $table->text('respect_and_accept');
-            $table->text('team_work');
-            $table->text('critical_thinking');
-            $table->text('imagination');
-            $table->text('open_to_change');
-            $table->text('ability_to_understand_the_content');
-            $table->text('ability_to_produce_art');
-            $table->text('ability_to_thinking');
-            $table->text('ability_to_inovate');
-            $table->timestamps();
+        Schema::create('trainer_reports', function (Blueprint $table) {
+            $table->increments('id');
+                $table->integer('event_id')->unsigned();
+                $table->foreign('event_id')->references('id')->on('events');
+                $table->integer('attendees_id')->unsigned();
+                $table->foreign('attendees_id')->references('id')->on('attendees');
+                $table->integer('trainer_id')->unsigned();
+                $table->foreign('trainer_id')->references('id')->on('trainers');
+                $table->integer('week');
+                $table->text('confidence');
+                $table->text('initiative');
+                $table->text('respect_and_accept');
+                $table->text('team_work');
+                $table->text('critical_thinking');
+                $table->text('imagination');
+                $table->text('open_to_change');
+                $table->text('ability_to_understand_the_content');
+                $table->text('ability_to_produce_art');
+                $table->text('ability_to_thinking');
+                $table->text('ability_to_inovate');
+                $table->timestamps();
+            });
     }
 
     /**
