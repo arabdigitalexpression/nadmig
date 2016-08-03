ab28dc40b83dd1a19d7322b829f493c7d71cc34f
Mohamed Mahrous <m.mahrous.94@gmail.com>
2016-08-03 02:50:41 +0200

    decrease number of words for list event

--- app/Modules/Event/Views/application/all.blade.php
+++ app/Modules/Event/Views/application/all.blade.php
@@ -14,7 +14,7 @@
                 <ul class="space-info">
                     <li><i class="fa fa-calendar" aria-hidden="true"></i> {{ ArabicDate($event->reservation->start_session['start_date']) }} </li>
                     <li><i class="fa fa-clock-o" aria-hidden="true"></i> من {{ ArabicTime($event->reservation->start_session['start_time']) }}
-                    <li>{{ str_limit($event->reservation->description, $limit = 50, $end = '...') }}</li>
+                    <p>{{ str_limit($event->reservation->description, $limit = 100, $end = '...') }}</p>
                 </ul>
                 
             </li>
