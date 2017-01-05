6b38284eab5e84e4fc6597c77ed0bb62a0d6d916
Mohamed Mahrous <m.mahrous.94@gmail.com>
2017-01-05 22:31:04 +0200

    home page lisiting

--- app/Http/Controllers/Application/HomeController.php
+++ app/Http/Controllers/Application/HomeController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers\Application;
 
 use App\Base\Controllers\ApplicationController;
-
+use App\Modules\Reservation\Models\Reservation;
+use Input;
 class HomeController extends ApplicationController
 {
     /**
@@ -13,7 +14,13 @@ class HomeController extends ApplicationController
      */
     public function index()
     {
-        
-        return view('application.home.index');
+     	$reservations = Reservation::Where('event_type', 'public')->where('status', 'accepted')->limit(15)->get();
+     	foreach ($reservations as $reservation) {
+     	    $reservation['start_date'] = $reservation->sessions[0]['start_date'];
+     	    $reservation['start_session'] = $reservation->sessions[0];
+     	}
+     	$reservations = $reservations->sortBy('start_date')->reverse();
+     	// dd($reservations->toArray());
+        return view('application.home.index', compact('reservations'));
     }
 }
--- resources/views/application/home/index.blade.php
+++ resources/views/application/home/index.blade.php
@@ -4,5 +4,37 @@
 @section('description'){{ getDescription() }}@endsection
 
 @section('content')
-    
+	<h3>تحت رجليك</h3>
+    <ul class="spaces-list">
+    	@for ($i = 0; $i < 3; $i++)
+    		@php($reservation = $reservations[$i])
+    	    <li class="panel panel-default panel-orange">
+                <a href="{{ route('event.page', ['event_slug' => $reservation->event->slug ]) }}">
+                    <div class="panel-heading">{{ $reservation->name }}</div>
+                </a>   
+                <div style="background-image: url({{ url($reservation->artwork) }});" class="space-icon"> </div>
+                <ul class="space-info">
+                    <li><i class="fa fa-calendar" aria-hidden="true"></i> {{ ArabicDate($reservation->start_session['start_date']) }} </li>
+                    <li><i class="fa fa-clock-o" aria-hidden="true"></i> من {{ ArabicTime($reservation->start_session['start_time']) }}
+                    <p>{{ str_limit($reservation->description, $limit = 100, $end = '...') }}</p>
+                </ul>
+            </li>
+    	@endfor
+        </ul>
+    	<h3>على مد الشوف</h3>
+        <ul class="spaces-list-small">
+        	@for ($i = 0; $i < 3; $i++)
+        		@php($reservation = $reservations[$i])
+        	    <li class="panel panel-default panel-orange">
+                    <a href="{{ route('event.page', ['event_slug' => $reservation->event->slug ]) }}">
+                        <div class="panel-heading">{{ $reservation->name }}</div>
+                    </a>   
+                    <div style="background-image: url({{ url($reservation->artwork) }});" class="space-icon"> </div>
+                    <ul class="space-info">
+                        <li><i class="fa fa-calendar" aria-hidden="true"></i> {{ ArabicDate($reservation->start_session['start_date']) }} </li>
+                        <li><i class="fa fa-clock-o" aria-hidden="true"></i> من {{ ArabicTime($reservation->start_session['start_time']) }}
+                    </ul>
+                </li>
+        	@endfor
+        </ul>
 @endsection
