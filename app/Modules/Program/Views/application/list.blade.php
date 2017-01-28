cfd018ad7cb7e9b3589ce0b012fb458f4706fdc1
Mohamed Mahrous <m.mahrous.94@gmail.com>
2017-01-28 16:50:55 +0200

    prgoram list

--- app/Modules/Program/Views/application/list.blade.php
+++ app/Modules/Program/Views/application/list.blade.php
@@ -8,14 +8,14 @@
         @foreach($programs as $program)
             <li class="panel panel-default panel-orange full">
                 <a href="{{ route('program.page', ['event_slug' => $program->slug ]) }}">
-                    <div class="panel-heading">{{ $program->name }}</div>
+                    <div class="panel-heading">{{ str_limit($program->name, $limit = 30, $end = '...') }}</div>
                 </a>   
                 <div style="background-image: url({{ url($program->artwork) }});" class="space-icon"> </div>
                 <ul class="space-info">
 
                     <li><i class="fa fa-calendar" aria-hidden="true"></i> من {{ ArabicDate($program->start_session['start_date']) }} </li>
                     <li> إلى {{ ArabicDate($program->end_session['start_date']) }} </li>
-                    <li>{{ str_limit($program->description, $limit = 150, $end = '...') }}</li>
+                    <li>{!! str_limit($program->description, $limit = 85, $end = '...') !!}</li>
                 </ul>
                 
             </li>
